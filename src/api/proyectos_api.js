6858ef58a9d7085ad6dc15a72bd906900396f284
lefasom1993 <lefasom1993@gmail.com>
2023-11-12 11:45:50 -0300

    add proyecto

--- src/api/proyectos_api.js
+++ src/api/proyectos_api.js
@@ -4,9 +4,9 @@ const proyectos = [
         name: "Chatea",
         src: "images/chatea3.png",
         cod: {
-            "front": ['https://github.com/lefasom/front-chatea','faCode'],
-            "back": ['https://github.com/lefasom/back-chatea','faCode'],
-            'demo': ["https://front-chatea.vercel.app/",'faLink'],
+            "front": ['https://github.com/lefasom/front-chatea', 'faCode'],
+            "back": ['https://github.com/lefasom/back-chatea', 'faCode'],
+            'demo': ["https://front-chatea.vercel.app/", 'faLink'],
         },
         tecno: "Vue - Vuex - Node - Firebase - MongoDB",
     },
@@ -15,8 +15,8 @@ const proyectos = [
         name: "La Consulta",
         src: "images/verduleria.png",
         cod: {
-            "front": ['https://github.com/lefasom/verduleria_la_consulta_next.js','faCode'],
-            'demo': ["https://verdulerialaconsultanext.vercel.app/",'faLink'],
+            "front": ['https://github.com/lefasom/verduleria_la_consulta_next.js', 'faCode'],
+            'demo': ["https://verdulerialaconsultanext.vercel.app/", 'faLink'],
         },
         tecno: "Next",
 
@@ -26,8 +26,8 @@ const proyectos = [
         name: "La galeria",
         src: "images/blog.png",
         cod: {
-            "front": ['https://github.com/lefasom/blogs-of-photo','faCode'],
-            "demo": ["https://blogs-of-photo-git-main-lefasom.vercel.app/",'faLink'],
+            "front": ['https://github.com/lefasom/blogs-of-photo', 'faCode'],
+            "demo": ["https://blogs-of-photo-git-main-lefasom.vercel.app/", 'faLink'],
         },
         tecno: "React - Redux - Firebase",
 
@@ -37,8 +37,8 @@ const proyectos = [
         name: "Clon de IG",
         src: "images/Instagram-clone.png",
         cod: {
-            "front": ['https://github.com/lefasom/clon-instagram','faCode'],
-            'demo': ["https://my-red-social.vercel.app/Perfil",'faLink'],
+            "front": ['https://github.com/lefasom/clon-instagram', 'faCode'],
+            'demo': ["https://my-red-social.vercel.app/Perfil", 'faLink'],
         },
         tecno: "React",
 
@@ -49,8 +49,8 @@ const proyectos = [
         name: "Control de precios",
         src: "images/apk.png",
         cod: {
-            "front": ['https://github.com/lefasom/crud-native','faCode'],
-            "Apk": ['https://expo.dev/artifacts/eas/2T8Y91k8CGEhD8KfJ7NNxV.apk','faLink'],
+            "front": ['https://github.com/lefasom/crud-native', 'faCode'],
+            "Apk": ['https://expo.dev/artifacts/eas/2T8Y91k8CGEhD8KfJ7NNxV.apk', 'faLink'],
         },
         tecno: "React Native - Firebase",
     },
@@ -59,8 +59,8 @@ const proyectos = [
         name: "La Serafina",
         src: "images/serafina.png",
         cod: {
-            "front": ['https://github.com/lefasom/la-serafina-2.0','faCode'],
-            'demo': ["https://la-serafina-2-0.vercel.app/",'faLink'],
+            "front": ['https://github.com/lefasom/la-serafina-2.0', 'faCode'],
+            'demo': ["https://la-serafina-2-0.vercel.app/", 'faLink'],
         },
         tecno: "Next - Firebase",
     }
@@ -70,8 +70,8 @@ const proyectos = [
         name: "Rec escolar",
         src: "images/rec-escolar.png",
         cod: {
-            "code": ['https://github.com/lefasom/recordatorio-escolar','faCode'],
-            'demo': ["https://notificacionesappescolar.000webhostapp.com/",'faLink'],
+            "code": ['https://github.com/lefasom/recordatorio-escolar', 'faCode'],
+            'demo': ["https://notificacionesappescolar.000webhostapp.com/", 'faLink'],
         },
         tecno: "PHP - MySql",
     }
@@ -81,8 +81,8 @@ const proyectos = [
         name: "Calculadora",
         src: "images/calculadora.png",
         cod: {
-            "code": ['https://github.com/lefasom/calculadora','faCode'],
-            'demo': ["https://calculadora-theta-gules.vercel.app//",'faLink'],
+            "code": ['https://github.com/lefasom/calculadora', 'faCode'],
+            'demo': ["https://calculadora-theta-gules.vercel.app//", 'faLink'],
         },
         tecno: "JavaScript - HTML - CSS",
     }
@@ -92,12 +92,22 @@ const proyectos = [
         name: "Gestor de negocio",
         src: "images/gestor.png",
         cod: {
-            "code": ['https://github.com/lefasom/gestion-de-negocio','faCode'],
-            "mp4": ['https://drive.google.com/file/d/1HsqSBHckmDiIbkKfDiiUZXs3C_3YRUs4/view?usp=sharing','faLink'],
+            "code": ['https://github.com/lefasom/gestion-de-negocio', 'faCode'],
+            "mp4": ['https://drive.google.com/file/d/1HsqSBHckmDiIbkKfDiiUZXs3C_3YRUs4/view?usp=sharing', 'faLink'],
         },
         tecno: "Node - ejs - MySql(XAMPP)",
     }
-
+    ,
+    {
+        id: 9,
+        name: "Lista de precios",
+        src: "images/python-crud.png",
+        cod: {
+            "code": ['https://github.com/lefasom/lista-de-precios', 'faCode'],
+            "demo": ['https://lista-de-precios-2v50.onrender.com', 'faLink'],
+        },
+        tecno: "Python - Flask - MongoDB - JavaScript",
+    }
 
 ];
 
